030278d23abb90a6cbb27c1a771d7fde041afe7d
Rishi Sayanthakumar <160595074+Christumas@users.noreply.github.com>
2025-06-08 11:12:34 +0200

    hamburger menu functionality implemented with animations

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// adding the functionality to the hamburger menu
+// adding the functionality to the hamburger menu for mobile
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar")
+const menuBackground = document.querySelector(".menuBackground")
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("change");
+    navbar.classList.toggle("change")
+    menuBackground.classList.toggle("change")
+
+
+})
