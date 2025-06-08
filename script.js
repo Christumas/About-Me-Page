e5214ad4babd0dda7537c9ae8568fabac7311d0b
Rishi Sayanthakumar <160595074+Christumas@users.noreply.github.com>
2025-06-08 19:45:06 +0200

    added the popping animation for the desktop view. Need to change the skills and interests bubbles to carousels for mobile version

--- script.js
+++ script.js
@@ -10,3 +10,49 @@ hamburger.addEventListener("click", () => {
 
 
 })
+
+
+// add the bubble popping effect to 
+const bubbles  = document.querySelectorAll(".bubble");
+console.log(bubbles)
+
+const dontPressButton = document.querySelector(".dontPress")
+let poppedCount = 0;
+const bubbleArray = Array.from(bubbles); //convert the nodelist to array 
+
+
+dontPressButton.addEventListener("click", ()=>{
+    const shuffled = [...bubbleArray].sort( () => { Math.random() -0.5})    //shuffle the array so the bubbles get popped in different orders
+    
+    //add the animation to each blob after a delay through settimeout
+    shuffled.forEach( (element) => {
+        const animationDelay = Math.random() * 2000 //generate a delay of about 2s
+
+        //the function adds the animation to each bubble after this generated delay
+        setTimeout( () => {
+            element.style.animation = "pop 1s ease-out forwards"
+
+            element.addEventListener("animationend", ()=> {
+                poppedCount++;
+
+                if(poppedCount == bubbleArray.length){
+                    setTimeout(() => { //bring back the bubbles after 2s 
+                        bubbleArray.forEach( (bubble) => {
+                            bubble.style.transform = "scale(1.0)";
+                            bubble.style.animation = "";
+                            bubble.style.opacity = 1;
+                            poppedCount = 0;
+                        })
+
+                    }, 2000)
+
+                }
+            }, {once:true})
+
+        },animationDelay)
+
+
+    })
+
+})
+
